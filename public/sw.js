5de7e7be8a32d9438e95aebc67b248e972d3e1b4
Priyanka Gulia <pgulia@horizontal.com>
2024-05-25 11:51:25 +0530

    Push Notification

--- public/sw.js
+++ public/sw.js
@@ -69,7 +69,7 @@ if (!self.define) {
 }
 define(['./workbox-8f0e986c'], (function (workbox) { 'use strict';
 
-  importScripts();
+  importScripts("worker-development.js");
   self.skipWaiting();
   workbox.clientsClaim();
   workbox.registerRoute("/", new workbox.NetworkFirst({
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import { Text, RichText, Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 import { ComponentProps } from 'lib/component-props';
+import PushNotification from './PushNotification';
 
 type HeaderProps = ComponentProps & {
   fields: {
@@ -13,47 +14,7 @@ type HeaderProps = ComponentProps & {
  * This is the most basic building block of a content site, and the most basic
  * JSS component that's useful.
  */
-const Header = ({ fields }: HeaderProps): JSX.Element => (
-  //   <div className="Header is-text-align-right is-masked-light">
-  //     <div className="media" style={{ backgroundImage: `url(priyanka-white.png)` }}></div>
-  //     <div className="content-wrap">
-  //       <div className="content">
-  //         <h1 className="text soft-pink">
-  //           Hi, I am Priyanka Senior Sitecore Developer based in Vadodara.
-  //         </h1>
-  //         <p>
-  //           I am specialized in the software development arena. For the last 6 years, I have been
-  //           developing web applications using C#, .NET MVC, JavaScript/jQuery, AngularJS and SQL
-  //           Server.
-  //         </p>
-  //         <p>
-  //           I{' '}
-  //           <a className="link-1" href="https://in.linkedin.com/in/priyanka-gulia" target="_blank">
-  //             work
-  //           </a>
-  //           , sometimes
-  //           <a
-  //             className="link-1"
-  //             href="https://www.upwork.com/freelancers/~01a9400badb5a2ec20"
-  //             target="_blank"
-  //           >
-  //             freelance
-  //           </a>{' '}
-  //           and push
-  //           <a className="link-1" href="https://github.com/priyanka-gulia" target="_blank">
-  //             code
-  //           </a>
-  //           .
-  //         </p>
-  //         <p>
-  //           For collaboration or questions;
-  //           <a className="link-2" href="mailto:priyankaguliya@gmail.com">
-  //             priyankaguliya@gmail.com
-  //           </a>
-  //         </p>
-  //       </div>
-  //     </div>
-  //   </div>
+const Header = ({}: HeaderProps): JSX.Element => (
   <div className="main">
     <div className="page-wrapper" style={{ backgroundImage: `url(priyanka-white.png)` }}></div>
     <div className="page-content">
@@ -72,6 +33,9 @@ const Header = ({ fields }: HeaderProps): JSX.Element => (
       <a className="link-2" href="mailto:priyankaguliya@gmail.com">
         priyankaguliya@gmail.com
       </a>
+    </div>
+    <div>
+      <PushNotification />
     </div>
   </div>
 );
